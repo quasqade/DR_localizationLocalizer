1aa3e59f6a4e6668176680c7bd136a8b66a49af0
quasqade <quasqade@gmail.com>
2017-09-01 22:37:35 +0300

    Reworked most of the algorithm. RandomAccessFile needs to go, too slow

--- src/Main.java
+++ src/Main.java
@@ -29,35 +29,58 @@ public class Main
 				preserveNewlines=true;
 		}
 
-		try
+		try //TODO: Probably should do something about this
 		{
-			byte[] data = Files.readAllBytes(path);
-			int startingPosition=0;
 
-			//DISGUSTING
-			for (int i = 6; i < data.length; i++)
+			//Open file for reading
+			RandomAccessFile inputFile = new RandomAccessFile(path.toString(), "r"); //avoid loading everything into memory
+			long startingPosition=0; //DR1 doesn't have archives exceeding 4GB, but better be safe
+
+			//Iterate through contents until "English" is found
+			System.out.println("Searching for English tag (this might take a long time)...");
+			for (long i = 0; i < inputFile.length()-6; i++)
 			{
-			byte[] tagByteArray = new byte[7];
-				tagByteArray[0]=data[i-6];
-				tagByteArray[1]=data[i-5];
-				tagByteArray[2]=data[i-4];
-				tagByteArray[3]=data[i-3];
-				tagByteArray[4]=data[i-2];
-				tagByteArray[5]=data[i-1];
-				tagByteArray[6]=data[i];
-
-				String tag = new String(tagByteArray, "UTF-8");
-				if (tag.equals("English"))
+				inputFile.seek(i);
+				//nested ifs to avoid moving pointer too much (probably more efficient than reading tag on each pass at cost of unnecessary double reading on partial matches)
+				if ((char)inputFile.readByte() == 'E')
 				{
-					startingPosition=i+1;
-					break;
+					if ((char)inputFile.readByte() == 'n')
+					{
+						if ((char)inputFile.readByte() == 'g')
+						{
+							if ((char)inputFile.readByte() == 'l')
+							{
+								if ((char)inputFile.readByte() == 'i')
+								{
+									if ((char)inputFile.readByte() == 's')
+									{
+										if ((char)inputFile.readByte() == 'h')
+										{
+											startingPosition = inputFile.getFilePointer();
+											System.out.println("Found English tag at " + Long.toHexString(startingPosition-7)+ ".");
+											break;
+										}
+									}
+								}
+							}
+						}
+					}
 				}
 			}
 
-			List<Integer> offsets = new ArrayList<>();
+			//Read and store string offsets in a list
+			List<Integer> offsets = new ArrayList<Integer>()
+				//delete this later
+			{
+				@Override
+				protected void finalize() throws Throwable {
+					System.out.println(this+" collected");
+					super.finalize();
+				}
+			};
 			int counter = 0;
 			byte[] offsetBytes = new byte[4];
-			for (int i=startingPosition; i<startingPosition+640; i++)
+			for (long i=startingPosition; i<startingPosition+640; i++)
 			{
 				if ((counter > 3)||(i==startingPosition+639))
 				{
@@ -65,40 +88,65 @@ public class Main
 					offsets.add(offsetInt);
 					counter =0;
 				}
-				offsetBytes[counter] = data[i];
+				inputFile.seek(i);
+				offsetBytes[counter] = inputFile.readByte();
 				counter++;
 			}
+
+			//Calculate differences between offsets (string lengths)
 			List<Integer> offsetDifferences = new ArrayList<>();
-for (int i = 0; i < offsets.size()-1; i++)
-{
-	offsetDifferences.add(offsets.get(i+1)-offsets.get(i));
-}
-			int currentByte=startingPosition+640;
+
+
+			for (int i = 0; i < offsets.size()-1; i++)
+			{
+				offsetDifferences.add(offsets.get(i+1)-offsets.get(i));
+			}
+
+			//Read and store strings in a list
+			long currentSeekerPos=startingPosition+640; //Not sure if its possible to add variables, but if it is, this should not be constant
 			List<String> strings = new ArrayList<>();
 			List<Byte> stringBytes = new ArrayList<>();
-			while (currentByte<data.length)
+			while (currentSeekerPos<inputFile.length())
 			{
-				if ((data[currentByte]==0))
+				inputFile.seek(currentSeekerPos);
+				byte currentByte = inputFile.readByte();
+				if (currentByte==0)
 				{
 					Byte[] stringByteArray = stringBytes.toArray(new Byte[stringBytes.size()]);
-					byte[] byteArray = new byte[stringByteArray.length];
+					byte[] byteArray = new byte[stringByteArray.length]; //TODO: basically duplicate array but with primitives, needs optimization
 					for(int i = 0; i < stringByteArray.length; i++) byteArray[i] = stringByteArray[i];
 					strings.add(new String(byteArray, "UTF-8"));
-					if (checkEndAhead(data, currentByte))
-					{
-						break;
-					}
-					stringBytes=new ArrayList<>();
+
+					//Look ahead for sequence of bytes 2,0,0,0 indicating end of English block
+					//Again nested ifs hopefully are more efficient
+						if (inputFile.readByte()==2)
+						{
+							if (inputFile.readByte()==0)
+							{
+								if (inputFile.readByte()==0)
+								{
+									if (inputFile.readByte()==0)
+									{
+										System.out.println("Found sequence 2000 at " + Long.toHexString(inputFile.getFilePointer()-4)+ ", terminating string search.");
+									break;
+									}
+								}
+							}
+						}
+					stringBytes=new ArrayList<>(); //possibly reuse instead of relying on GC? But unlikely to be more efficient
 				}
 				else
 				{
-					stringBytes.add(data[currentByte]);
+					stringBytes.add(currentByte);
 				}
-				currentByte++;
+				currentSeekerPos++;
 			}
 
-			int endingPosition = currentByte+1;
 
+
+			long endingPosition = currentSeekerPos+1;
+
+			//Sanity check to at least make sure all string match their offsets, could be just a waste of resources though
 			counter=2;
 			boolean mismatchFlag=false;
 			for (String string: strings
@@ -114,38 +162,48 @@ for (int i = 0; i < offsets.size()-1; i++)
 			}
 			if (mismatchFlag)
 			{
-				System.out.println("Offsets mismatch string! Exiting...");
+				System.out.println("Offsets mismatch string! Exiting..."); //TODO: should throw exception instead
 				return;
 			}
 
+			//Writing editable file
 			System.out.println("Offsets look good! Proceeding to write editable string file...");
-
-			Writer out = new BufferedWriter(new OutputStreamWriter(
-					new FileOutputStream("EDIT THIS FILE IN NPP.txt"), "UTF-8"));
-			try {
-				for (String string: strings
-					 )
+			List<String> translatedStrings;
+			File fileDir;
+			BufferedReader in;
+			String str;
+			int[] translatedLengths;
+			File file;
+			FileOutputStream fos;
+			int currentOffset;
+			try (Writer out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("EDIT THIS FILE IN NPP.txt"), "UTF-8")))
+			{
+				try
 				{
-					out.write(string);
-					out.write("\n");
+					for (String string : strings)
+					{
+						out.write(string);
+						out.write("\n");
+					}
+				} finally
+				{
+					out.close();
 				}
-			} finally {
-				out.close();
 			}
 
+			//Waiting for user input
 			System.out.println("Sucessfully wrote editable file. Open it in Notepad++ and translate strings. When done, press Enter...");
 			System.in.read();
 			System.out.println("Processing strings...");
 
-			List<String> translatedStrings = new ArrayList<>();
+			translatedStrings = new ArrayList<>();
 
-			File fileDir = new File("EDIT THIS FILE IN NPP.txt");
+			//Reading edited file
+			//TODO: add validity checks?
+			//TODO: test new variables
+			fileDir = new File("EDIT THIS FILE IN NPP.txt");
 
-			BufferedReader in = new BufferedReader(
-					new InputStreamReader(
-							new FileInputStream(fileDir), "UTF8"));
-
-			String str;
+			in = new BufferedReader(new InputStreamReader(new FileInputStream(fileDir), "UTF8"));
 
 			while ((str = in.readLine()) != null) {
 
@@ -157,9 +215,12 @@ for (int i = 0; i < offsets.size()-1; i++)
 
 			in.close();
 
-			System.out.println("Calculating new string lengths...");
 
-			int[] translatedLengths = new int[translatedStrings.size()];
+			//Create array of string lengths
+			//TODO: possibly rework without new list?
+			//TODO: use list like everywhere else
+			System.out.println("Calculating new string lengths...");
+			translatedLengths = new int[translatedStrings.size()];
 			counter=0;
 			for (String transString: translatedStrings
 				 )
@@ -167,82 +228,86 @@ for (int i = 0; i < offsets.size()-1; i++)
 				translatedLengths[counter]=transString.getBytes("UTF-8").length+1;
 				counter++;
 			}
-			List<Byte> outputBytes = new ArrayList<>();
 
+
+			System.out.println("Starting writing output file... (this might take a long time and process might appear frozen)");
+
+			//Start writing output file
+			//TODO: delegate to another thread and display progress
+			if (args.length>0)
+			{
+				int lastDot = args[0].lastIndexOf('.');//find where extension starts
+				file = new File(args[0].substring(0, lastDot) + "(edited)" + args[0].substring(lastDot)); //append (edited) to output filename
+			}
+			else
+				file = new File("dr_localization(edited).bin");
+
+			fos=new FileOutputStream(file);
+
+			//TODO: this might be very inefficient, need to examine
 			for (int i = 0; i<startingPosition;i++)
 			{
-				outputBytes.add(data[i]);
+				inputFile.seek(i);
+				fos.write(inputFile.readByte());
 			}
 
-			int currentOffset=0;
-
-			System.out.println("Calculating new offsets...");
+			//Calculate and write new offsets
+			currentOffset = 0;
+			System.out.println("Writing new offsets...");
 			for (int i=0; i<translatedLengths.length+3; i++)
 			{
 				if (i<3)
 				{
-currentOffset=offsets.get(i);
+				currentOffset=offsets.get(i);
 				}
 				else
 				{
 					currentOffset += translatedLengths[i-3];
 				}
+
+				//convert to LE byte array
 				ByteBuffer dbuf = ByteBuffer.allocate(4);
 				dbuf.putInt(currentOffset);
 				dbuf.flip();
 				byte[] result = dbuf.array();
 				for (int j = result.length-1; j>=0; j--)
 				{
-					outputBytes.add(result[j]);
+					//No idea why the fuck was it working before without this, some FOS weirdness:
+					int realResult = result[j]& 0xFF;
+					String realResultHex = Integer.toHexString(realResult);
+					fos.write(result[j]& 0xFF);
 				}
 
 			}
-			System.out.println("Creating bytes for new strings...");
 
+			//Calculate and write new strings
+			System.out.println("Writing new strings...");
 			for (String transString: translatedStrings
 					)
 			{
 				byte[] transStringBytes = transString.getBytes("UTF-8");
 				for (int i = 0; i<transStringBytes.length; i++)
 				{
-					outputBytes.add(transStringBytes[i]);
+					fos.write(transStringBytes[i]);
 				}
-				outputBytes.add((byte)0);
+				fos.write((byte)0);
 			}
 
-			for (int i=endingPosition; i<data.length; i++)
+			for (long i=endingPosition; i<inputFile.length(); i++)
 			{
-				outputBytes.add(data[i]);
+				inputFile.seek(i);
+				fos.write(inputFile.readByte());
 			}
 
-			int k = 0;
-			byte[] outputByteArray = new byte[outputBytes.size()];
-			for (Byte b: outputBytes
-					)
-			{
-			outputByteArray[k++]=b;
-			}
-			System.out.println("Writing output bin...");
 
-			File file;
-
-			if (args.length>0)
-				file = new File(args[0]);
-			else
-				file = new File("dr_localization.bin");
-			Files.deleteIfExists(file.toPath());
-
-			FileOutputStream fos;
-			if (args.length>0)
-				fos=new FileOutputStream(args[0]);
-			else
-				fos = new FileOutputStream("dr_localization.bin");
-
-			fos.write(outputByteArray);
+			inputFile.close();
 			fos.close();
 
+			System.out.println("Deleting temp file");
 			file = new File("EDIT THIS FILE IN NPP.txt");
 			Files.deleteIfExists(file.toPath());
+
+			System.out.println("Done!");
 
 		} catch (Exception e)
 		{
@@ -251,20 +316,5 @@ currentOffset=offsets.get(i);
 
 
 	}
-
-public static boolean checkEndAhead(byte[] data, int currentByte)
-{
-	//DISGUSTING
-	boolean end = true;
-	if (data[currentByte+1]!=(byte)2)
-		end=false;
-	if (data[currentByte+2]!=(byte)0)
-		end=false;
-	if (data[currentByte+3]!=(byte)0)
-		end=false;
-	if (data[currentByte+4]!=(byte)0)
-		end=false;
-	return end;
-}
 }
 
